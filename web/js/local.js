af75d5baa5c14c16d82a09d1d0192b655a048cfd
Jacob Paine <painejacob@gmail.com>
2016-10-20 11:55:24 -0500

    Adds all assets, and most transitions.

---
+++ web/js/local.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+  $('.home').click(function() {
+// fade out anything
+    $(".top-img").fadeOut(400);
+    $(".bottom-left").fadeOut(400);
+    $(".bottom-right").fadeOut(400);
+
+// fade in specific
+    $(".home-top-img").fadeToggle(500);
+    $(".home-bottom-right").fadeToggle(500);
+    $(".home-bottom-left").fadeToggle(500);
+
+  });
+
+  $('.menu').click(function() {
+    $(".home-bottom-left").fadeToggle();
+    $(".home-bottom-right").fadeToggle();
+    $(".home-top-img").fadeToggle();
+  });
+
+  $('.hours').click(function() {
+    $(".top-img").fadeOut(400);
+    $(".bottom-left").fadeOut(400);
+    $(".bottom-right").fadeOut(400);
+
+    $(".hours-top-img").fadeToggle(500);
+    $(".hours-bottom-left").fadeToggle(500);
+    $(".hours-bottom-right").fadeToggle(500);
+  });
+
+  $('.mission').click(function() {
+    $(".top-img").fadeOut(400);
+    $(".bottom-left").fadeOut(400);
+    $(".bottom-right").fadeOut(400);
+
+    $(".mission-top-img").fadeToggle(500);
+    $(".mission-bottom-left").fadeToggle(500);
+    $(".mission-bottom-right").fadeToggle(500);
+
+  });
+
+});
